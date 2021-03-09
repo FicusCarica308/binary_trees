f048a2b5f1933150a124e30ff04bf34d163cfe06
Ethan-23 <emanroberts@gmail.com>
2021-03-09 03:00:18 +0000

    added task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: given node to check
+ * Return: 1 for leaf and 0 if not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	return (0);
+}
